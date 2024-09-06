303d1d3fe78053c826761b8ddc6b8343ae00ea84
moniar412 <114915996+moniar412@users.noreply.github.com>
2024-09-06 16:24:44 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # nestedIDR
+**Description**: The package fits the idr model for hierarchical structured data (e.g ChIP-seq data from different labs with multiple replicates) to measure the reproducibility and replicability of findings from replicate experiments in multi-source studies. Using a nested copula mixture model that characterizes the interdependence between replication experiments both across and within sources, the hierarchical idr quantifies reproducibility and replicability of each candidate simultaneously in a coherent framework.
+
+This project reports R code for implementing the methods and reproducing the results in Ranalli et al. "A statistical framework for measuring reproducibility and replicability of high-throughput experiments from multiple sources".
+
+The following libraries shall be installed in R before using code from
+this project:
+
+library(mixtools)
+
+library(idr)
+
---
+++ crt_simusetting.R
@@ -0,0 +1,103 @@
+
+rm(list=ls(all=TRUE));
+
+source("funs_general.R")
+source("funs/funs_nested_idr.R")
+
+## setting
+
+simu_setting <- list()
+simu_setting$id <- 4
+simu_setting$tag <- NULL
+simu_setting$randSeed <- 1
+simu_setting$simuTimes <- 200
+simu_setting$sampleSize <- 5000
+
+simu_setting$NSPcutn <- seq(1,5000,50)
+
+simu_setting$clcut <- 0.5
+
+
+## true parameters
+#M  <- 2                                 #number of labs
+#R  <- c(2,2)                            #number of reps in each lab
+
+tpar <- list()
+# g:1-signal 0-noise
+# k:1-repducible 0-irre
+
+tpar$pi_g <- c(0.5,0.5)                      #pi g=0,1               
+tpar$pi_k <- rbind(c(0.2,0.8),c(0.5,0.5))    #pi k=0,1|g=1 for each lab
+tpar$mu    <- c(2, 2)                         #mu k=1|g=1 for each lab
+tpar$mu_k0 <- c(1, 1)                         #mu k=0|g=1 for each lab
+tpar$sgm <- c(1, 1.4)                        #sigma2 k=1|g=1 for each lab
+tpar$rho  <- c(0.9, 0.7)                    #rho k=1|g=1 for each lab
+
+## nlminb bounds
+nlm_Bound <- list()
+################ pi_g1,pi_g1k1m1,pi_g1kim2,mu_k1m1,mu_k1m2,mu_k0m1,mu_k0m2,sgm_k1m1,sgm_k1m2,rho_k1m1,rho_k2m2
+nlm_Bound$low <- c(0.05, 0.05, 0.05, 0.5, 0.5, 0.5, 0.5, 0.05, 0.05, -0.99, -0.99)
+nlm_Bound$up  <- c(0.95, 0.95, 0.95,  20,  20,  20,  20,   10,   10,  0.99,  0.99)
+nlm_control <- list(eval.max=200,iter.max=100,trace=0,rel.tol=0.01)
+
+out_control <- list()
+out_control$verbose <- list(basic=F,par=F) # basic, par
+out_control$iterMax <- 100
+out_control$eps_loglik <- 0.01
+out_control$eps_parVec <- c(0.001, 0.001, 0.001, 0.001, 0.001, 0.001, 0.001, 0.001, 0.001, 0.001, 0.001)
+
+#
+###
+
+
+## par0 for different method
+
+tpar
+
+par0.idrm <- par_list2vec(tpar)
+
+par0.idra <- list()
+par0.idra$muRange <- c(sum(tpar$mu)/2+0.2,sum(tpar$mu)/2-0.2)
+par0.idra$sgmRange <- c(sum(tpar$sgm)/2+0.1,sum(tpar$sgm)/2-0.1)
+par0.idra$rhoRange <- c(sum(tpar$rho)/2-0.05,sum(tpar$rho)/2+0.05)
+par0.idra$p0Range <-  c(min(tpar$pi_k[,2]*tpar$pi_g[2]-0.1),max(tpar$pi_k[,2]*tpar$pi_g[2]+0.1))
+
+par0.idrs <- list()
+par0.idrs$mu <- c(3,3)
+par0.idrs$sgm <- tpar$sgm
+par0.idrs$rho <- tpar$rho
+par0.idrs$p <- tpar$pi_k[,2]*tpar$pi_g[2]
+
+
+##
+
+save(file = paste("simulation/data/smset_",simu_setting$id,".Rdata",sep=""),
+     simu_setting,tpar,nlm_Bound,nlm_control,out_control,par0.idra,par0.idrm,par0.idrs)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
